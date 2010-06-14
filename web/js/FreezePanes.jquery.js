527601d8d0e96c56b7ce0299467f903fda7aa41b
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2010-06-14 09:55:58 +0000

    [sfDatagridPlugin] lombardot Add FreezePanes.jquery.js to freeze panes with jquery like Excel

---
+++ web/js/FreezePanes.jquery.js
@@ -0,0 +1,28 @@
+/**
+freeze panes / jquery Datagrid
+**/
+$(document).ready(function(){
+    cols= $('.grid').find('tr.lt:first').find('td').length;
+    c_width=new Array();
+    $('.grid').find('tr td.filter').each(function(i){
+    	c_width[i]=$(this).width();
+    	$(this).attr('style','width:'+(c_width[i])+'px');
+    });
+    $('.grid').find('tr th').each(function(i){
+    	$(this).attr('style','width:'+(c_width[i])+'px');
+    });
+	$('.grid tbody').append('<tr><td style="padding:0; border:0 none;" colspan="'+cols+'"><div class="grid-fixed-height"><table cellspacing="1" border="0" style="width:100%" cellspadding="0"></table></div></td></tr>');
+	$('.grid').find('tr').each(function(){
+			if($(this).hasClass('lt') || $(this).hasClass('dr')){
+				//Resize cols
+				$(this).find('td').each(function(i){
+					$(this).attr('style','width:'+(c_width[i])+'px');
+				});
+				//Replace row
+				$(this).parent().parent().find('.grid-fixed-height table').append($(this));
+			}
+		})
+	;
+	
+	
+});
