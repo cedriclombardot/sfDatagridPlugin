65909303ee2a6387d035a528453ffb211fa8c7b3
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2010-06-18 12:20:47 +0000

    [sfDatagridPlugin] Fix reset on IE with Jquery

--- web/js/jquery.datagrid.js
+++ web/js/jquery.datagrid.js
@@ -24,16 +24,7 @@ function dg_send(form, datagridName, type, url, freeze_after)
             break;
         
         case 'reset':
-        	oForm=document.getElementById(form);
-        	el=oForm.elements;
-        
-        	
-        	for(i in el){
-        		
-        			el[i].value='';
-        			
-        		
-        	}
+        	clearForm(oForm)
         	url+='&reset=1';
         	dg_send(form, datagridName, 'search', url, freeze_after);
             break;
@@ -88,3 +79,30 @@ function function_exists (function_name) {
         return (function_name instanceof Function);
     }
 }
+
+/**
+@see: http://www.learningjquery.com/2007/08/clearing-form-data
+**/
+function clearForm(form) {
+  // iterate over all of the inputs for the form
+  // element that was passed in
+  $(':input', form).each(function() {
+ var type = this.type;
+ var tag = this.tagName.toLowerCase(); // normalize case
+ // it's ok to reset the value attr of text inputs,
+ // password inputs, and textareas
+ if (type == 'text' || type == 'password' || tag == 'textarea')
+   this.value = "";
+ // checkboxes and radios need to have their checked state cleared
+ // but should *not* have their 'value' changed
+ else if (type == 'checkbox' || type == 'radio')
+   this.checked = false;
+ // select elements need to have their 'selectedIndex' property set to -1
+ // (this works for both single and multiple select elements)
+ else if (tag == 'select')
+   this.selectedIndex = -1;
+  });
+};
+
+
+
